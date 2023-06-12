7f32636aa669ef77463b3ae583dff2e2fcd3f119
Yiğit Can TEKE <yigitcanteke@Oremas-iMac.local>
2023-06-12 13:29:26 +0300

    added wrong path handler, updated auth validators

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(cors());
 app.use(todoRouter);
 app.use(authRouter);
 
+app.all('*', (req, res, next) => {
+    res.status(404).json({ error: 'Not Found' });
+});
+
 
 (async () => {
     try {
